d20f1d9778dd7c886c729ee46bacf82442426919
Abdus Samad <abdussamad018@gmail.com>
2024-09-14 02:11:55 +0800

    project testing

--- app/page.js
+++ app/page.js
@@ -1,30 +1,80 @@
-import { Grid2, Stack } from "@mui/material";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import BasicLineChart from "./components/chart/lineChart";
+import * as React from 'react';
+import { createTheme, ThemeProvider, alpha } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import getDashboardTheme from './theme/getDashboardTheme';
+import AppNavbar from './components/AppNavbar';
+import Header from './components/Header';
+import MainGrid from './components/MainGrid';
+import SideMenu from './components/SideMenu';
+import TemplateFrame from './TemplateFrame';
 
-export default function About() {
-  return (
-    <Container maxWidth="lg">
-      <Grid2 container spacing={2} marginBottom={5} direction={'row'} marginTop={5}>
-        <Grid2 size={6} border={2}>
-          <BasicLineChart></BasicLineChart>
-        </Grid2>
+export default function Dashboard() {
+    const [mode, setMode] = React.useState('light');
+    const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+    const dashboardTheme = createTheme(getDashboardTheme(mode));
+    const defaultTheme = createTheme({ palette: { mode } });
+    // This code only runs on the client side, to determine the system color preference
+    React.useEffect(() => {
+        // Check if there is a preferred mode in localStorage
+        const savedMode = localStorage.getItem('themeMode');
+        if (savedMode) {
+            setMode(savedMode);
+        } else {
+            // If no preference is found, it uses system preference
+            const systemPrefersDark = window.matchMedia(
+                '(prefers-color-scheme: dark)',
+            ).matches;
+            setMode(systemPrefersDark ? 'dark' : 'light');
+        }
+    }, []);
 
-        <Grid2 size={6} border={2}>
-          <BasicLineChart></BasicLineChart>
-        </Grid2>
-      </Grid2>
-      <Stack
-        direction={"row"}
-        spacing={2}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Button color="info" variant="contained">
-          Start
-        </Button>
-      </Stack>
-    </Container>
-  );
+    const toggleColorMode = () => {
+        const newMode = mode === 'dark' ? 'light' : 'dark';
+        setMode(newMode);
+        localStorage.setItem('themeMode', newMode); // Save the selected mode to localStorage
+    };
+
+    const toggleCustomTheme = () => {
+        setShowCustomTheme((prev) => !prev);
+    };
+
+    return (
+        <TemplateFrame
+            toggleCustomTheme={toggleCustomTheme}
+            showCustomTheme={showCustomTheme}
+            mode={mode}
+            toggleColorMode={toggleColorMode}
+        >
+            <ThemeProvider theme={showCustomTheme ? dashboardTheme : defaultTheme}>
+                <CssBaseline enableColorScheme />
+                <Box sx={{ display: 'flex' }}>
+
+                    {/* Main content */}
+                    <Box
+                        component="main"
+                        sx={(theme) => ({
+                            flexGrow: 1,
+                            backgroundColor: alpha(theme.palette.background.default, 1),
+                            overflow: 'auto',
+                        })}
+                    >
+                        <Stack
+                            spacing={2}
+                            sx={{
+                                alignItems: 'center',
+                                mx: 3,
+                                pb: 10,
+                                mt: { xs: 8, md: 0 },
+                            }}
+                        >
+                            <Header />
+                            <MainGrid />
+                        </Stack>
+                    </Box>
+                </Box>
+            </ThemeProvider>
+        </TemplateFrame>
+    );
 }
